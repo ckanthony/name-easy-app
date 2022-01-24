d8ca93a18ea80fa8d8bcb6aafd56fe999d822982
Anthony Chan <ckanthony@gmail.com>
2022-01-24 15:48:18 +0000

    add pageview

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import styles from './App.css';
 import ReactGA from 'react-ga';
 
 ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
-
-
+ReactGA.pageview(window.location.pathname);
 
 export default class Application extends Component {
   render() {
